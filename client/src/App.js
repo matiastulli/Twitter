b876d83d9e474e5efeef51c9ef15e1524943f0bb
Juan Matias <jmatiastulli@gmail.com>
2022-07-14 16:48:15 -0300

    {update} views of login

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,33 @@
-import React from "react";
- 
-// We use Route in order to define the different routes of our application
-import { Route, Routes } from "react-router-dom";
- 
-// We import all the components we need in our app
-import Navbar from "./components/navbar";
-import RecordList from "./components/recordList";
-import Edit from "./components/edit";
-import Create from "./components/create";
- 
-const App = () => {
- return (
-   <div>
-     <Navbar />
-     <Routes>
-       <Route exact path="/" element={<RecordList />} />
-       <Route path="/edit/:id" element={<Edit />} />
-       <Route path="/create" element={<Create />} />
-     </Routes>
-   </div>
- );
-};
- 
+import Homepage from "./components/homepage/homepage"
+import Login from "./components/login/login"
+import Register from "./components/register/register"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+
+} from "react-router-dom";
+import { useState } from 'react';
+function App() {
+  const [user, setLoginUser] = useState({
+
+  })
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route exact path="/">
+            {
+              user && user._id ? <Homepage /> : <Login />
+            }<Homepage /></Route>
+          <Route path="/login"><Login setLoginUser={setLoginUser} /></Route>
+          <Route path="/register"><Register /></Route>
+        </Routes>
+
+      </Router>
+
+    </div>
+  );
+}
+
 export default App;
